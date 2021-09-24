c73b292598545fabf5fcd8720dfe45e734029415
Gagan Diwan <gagan.diwan@icloud.com>
2021-09-24 19:13:44 +0300

    Simple Palindrome checker + using while loop

--- Palindrome.py
+++ Palindrome.py
@@ -12,7 +12,26 @@ for i in input_string[::-1]:
     reversed_string += i
 
 if reversed_string == input_string:
-    print("{}} is a Palindrome".format(input_string))
+    print("{} is a Palindrome".format(input_string))
 else:
     print("{} is not a Palindrome".format(input_string))
+
+# Using while loop
+
+message = "amma".lower()
+start_index = 0
+end_index = len(message) -1
+is_palindrome = True
+
+while (start_index <= end_index):
+    if message[start_index] != message[end_index]:
+        is_palindrome = False
+        break
+    start_index += 1
+    end_index -= 1
+
+if is_palindrome:
+    print("{} is a Palindrome".format(message))
+else:
+    print("{} is not a Palindrome".format(message))
 
